132d241244a7b34d9ea74c7c8e9c7d1d2d78075f
Matthias Pfefferle <pfefferle@gmail.com>
2020-07-22 16:26:38 +0200

    initial

---
+++ templates/feed-twtxt.php
@@ -0,0 +1,33 @@
+# | |___      _| |___  _| |_
+# | __\ \ /\ / / __\ \/ / __|
+# | |_ \ V  V /| |_ >  <| |_
+#  \__| \_/\_/  \__/_/\_\\__|
+#
+# Twtxt is an open, distributed
+# microblogging platform that
+# uses human-readable text files,
+# common transport protocols, and
+# free software.
+#
+# Learn more about twtxt at
+#   https://github.com/buckket/twtxt
+#
+# Learn more about the WordPress plugin
+#   https://github.com/pfefferle/wordpress-twtxt
+#
+# ------------------------------------------
+#
+#    nick = <?php echo twtxt_get_nick() . PHP_EOL; ?>
+#    url = <?php self_link(); echo PHP_EOL; ?>
+#    lang = <?php echo get_locale() . PHP_EOL; ?>
+
+<?php
+while ( have_posts() ) :
+	the_post();
+
+	echo mysql2date( 'c', get_post_time( 'Y-m-d H:i:s', true ), false );
+	echo "\t";
+	echo twtxt_get_the_excerpt();
+	echo PHP_EOL;
+
+endwhile;
---
+++ twtxt.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Plugin Name: TWTXT
+ * Plugin URI: https://github.com/pfefferle/wordpress-twtxt
+ * Description: Twtxt is an open, distributed microblogging platform
+ * Author: Matthias Pfefferle
+ * Author URI: https://notiz.blog
+ * Version: 1.0.0
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ * Text Domain: twtxt
+ */
+
+register_activation_hook( __FILE__, 'twtxt_flush_rewrite_rules' );
+register_deactivation_hook( __FILE__, 'twtxt_flush_rewrite_rules' );
+
+/**
+ * Init function
+ */
+function twtxt_init() {
+	add_filter( 'feed_content_type', 'twtxt_feed_content_type', 10, 2 );
+	add_feed( 'twtxt', 'do_feed_twtxt' );
+	add_action( 'do_feed_twtxt', 'do_feed_twtxt', 10, 1 );
+}
+add_action( 'init', 'twtxt_init' );
+
+/**
+ * Adds "twtxt" content-type
+ *
+ * @param string $content_type the default content-type
+ * @param string $type the feed-type
+ *
+ * @return string the as1 content-type
+ */
+function twtxt_feed_content_type( $content_type, $type ) {
+	if ( 'twtxt' === $type ) {
+		return 'text/plain';
+	}
+
+	return $content_type;
+}
+
+/**
+ * Adds an twtxt json feed
+ */
+function do_feed_twtxt( $for_comments ) {
+	if ( ! $for_comments ) {
+		// load post template
+		load_template( dirname( __FILE__ ) . '/templates/feed-twtxt.php' );
+	}
+}
+
+/**
+ * Reset rewrite rules
+ */
+function twtxt_flush_rewrite_rules() {
+	global $wp_rewrite;
+	$wp_rewrite->flush_rules();
+}
+
+/**
+ * Retunr the excerpt of the blogpost
+ *
+ * @param integer $length The length of the text.
+ *
+ * @return string The shortened text.
+ */
+function twtxt_get_the_excerpt( $length = 135 ) {
+	$excerpt        = get_the_excerpt();
+	$excerpt_length = apply_filters( 'excerpt_length', $length );
+	$excerpt_more   = apply_filters( 'excerpt_more', ' [...]' );
+
+	return wp_trim_words( $excerpt, $excerpt_length, $excerpt_more );
+}
+
+/**
+ * Return the nickname of the feed owner.
+ *
+ * @return string The nickname.
+ */
+function twtxt_get_nick() {
+	if ( is_author() ) {
+		return sanitize_title( get_the_author_meta( 'user_nicename' ) );
+	}
+
+	return sanitize_title( get_bloginfo( 'name' ) );
+}
